2efb08fb0a3e62bee184cc46d43a3b74a48d97d2
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-12-30 19:23:44 +0900

    DLLs included

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as r:
+  long_description=r.read()
+URL="https://github.com/KoichiYasuoka/mecab-cygwin"
+
+setuptools.setup(
+  name="mecab-cygwin",
+  version="0.3.0",
+  description="MeCab Python for Cygwin",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url=URL,
+  author="Koichi Yasuoka",
+  author_email="yasuoka@kanji.zinbun.kyoto-u.ac.jp",
+  license="GPL",
+  keywords="mecab nlp",
+  packages=setuptools.find_packages(),
+  install_requires=[],
+  python_requires=">=3.6",
+  package_data={
+    "MeCab":["./mecabrc.in","./dic/*"],
+  },
+  classifiers=[
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Other",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Text Processing :: Linguistic",
+  ],
+  project_urls={
+    "mecab-python3":"https://pypi.org/projects/mecab-python3/",
+    "source":URL,
+    "Tracker":URL+"/issues",
+  }
+)
---
+++ upload.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+rm -fr dist *.egg-info
+for D in libs/*
+do rm -fr build
+   mkdir -p build/lib/MeCab
+   cp $D/* build/lib/MeCab
+   python3 setup.py bdist_wheel
+   cd dist
+   F=`ls -1t *.whl | head -1`
+   case $F in
+   mecab_cygwin-*.whl) B=`expr $F : '\(mecab_cygwin-[0-9.]*-\)'` ;;
+   *) exit 1 ;;
+   esac
+   echo mv $F $B`basename $D`.whl | sh -x
+   cd ..
+done
+twine upload --repository pypi dist/*
+exit 0
